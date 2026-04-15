d10229b2beacee2497307f875e851913a87abeb0
Cyrille Etobe <cyrilleetobe@gmail.com>
2026-04-15 16:27:37 +0100

    Mise à jour du nom dans l'application

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
         <head><title>Mon App Flask</title></head>
         <body style="background-color: #f0f2f5; font-family: Arial; text-align: center; padding-top: 50px;">
             <h1 style="color: #2c3e50;">Bonjour à tous !</h1>
-            <p style="font-size: 20px;">Ceci est une simple application conteneurisée avec Docker par <strong>TonNom</strong>!</p>
+            <p style="font-size: 20px;">Ceci est une simple application conteneurisée avec Docker par <strong>Cyrille Etobe</strong>!</p>
         </body>
     </html>
     """
